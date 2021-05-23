5205df49753eb31bdd0520c6dc566fcfeaf9509c
Max Cohen <max.zagouri@pm.me>
2021-05-23 17:45:52 +0200

    [config] set default term to st

--- config.py
+++ config.py
@@ -29,10 +29,9 @@ from typing import List  # noqa: F401
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
 
 mod = "mod4"
-terminal = guess_terminal()
+terminal = "st"
 
 keys = [
     # Switch between windows
